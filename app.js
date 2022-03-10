3f31bf9cc9ddbb7750bf4ece43a544bff11b2d7b
wanradavich <wrda.ruks@gmail.com>
2022-03-10 14:56:44 +0700

    Try bcrypt

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const req = require("express/lib/request");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express()
-
-
-console.log(process.env);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -25,7 +24,7 @@ const userSchema = new mongoose.Schema ({
 
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,16 +42,18 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User ({
-        email:req.body.username,
-        password:req.body.password
-    });
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email:req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if (err){
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 
@@ -65,11 +66,11 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if(foundUser){
-                if (foundUser.password === password){
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                });
                     res.render("secrets");
                 }
             }
-        }
     });
 });
 
